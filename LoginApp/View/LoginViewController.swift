0a43d55f3e8ed0d7cc0fd2877d92e7b2cad528c7
Manohin <111537419+Manohin@users.noreply.github.com>
2023-02-13 12:54:24 +0300

    "Forgot buttons" added

--- LoginApp/View/LoginViewController.swift
+++ LoginApp/View/LoginViewController.swift
@@ -15,5 +15,35 @@ class LoginViewController: UIViewController {
     }
 
 
+    @IBAction func forgotLoginButtonTapped(_ sender: UIButton) {
+        present(
+            getAlertMessage(
+                title: "Ой!",
+                message: "Имя пользователя: 111"
+            ),
+            animated: true
+        )
+    }
+    
+    @IBAction func forgotPassButtonTapped(_ sender: UIButton) {
+        present(
+            getAlertMessage(
+                title: "Ой!",
+                message: "Пароль: 111"),
+            animated: true)
+    }
 }
 
+private func getAlertMessage(title: String, message: String) -> UIAlertController {
+    let alert = UIAlertController(
+        title: title,
+        message: message,
+        preferredStyle: .alert
+    )
+    alert.addAction(UIAlertAction(
+        title: "ОК",
+        style: .cancel,
+        handler: nil)
+    )
+    return alert
+}
