3477b68060d54faffc90d819aa7c2badbaa955a6
Manohin <111537419+Manohin@users.noreply.github.com>
2023-02-13 23:51:08 +0300

    Update

--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -16,27 +16,7 @@ final class WelcomeViewController: UIViewController {
         super.viewDidLoad()
         
         welcomeLabel.text = "Привет, \(userName ?? "")!"
-        
-        let firstColor = UIColor(
-            red: 255 / 255,
-            green: 153 / 255,
-            blue: 255 / 255,
-            alpha: 1).cgColor
-        
-        let secondColor = UIColor(
-            red: 102 / 255,
-            green: 102 / 255,
-            blue: 255 / 255,
-            alpha: 1).cgColor
-        
-        let gradientLayer = CAGradientLayer()
-        
-        gradientLayer.frame = view.bounds
-        gradientLayer.colors = [firstColor, secondColor]
-        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
-        gradientLayer.endPoint = CGPoint(x: 0, y: 1)
-        
-        view.layer.insertSublayer(gradientLayer, at: 0)
+        applyGradient(view: super.view)
     }
     
     @IBAction func logOutButtonTapped() {
