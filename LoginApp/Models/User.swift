f2ccb83d0807cd0fb8f50af871991d286faebe73
Manohin <111537419+Manohin@users.noreply.github.com>
2023-02-17 22:51:12 +0300

    Minor fix

--- LoginApp/Models/User.swift
+++ LoginApp/Models/User.swift
@@ -5,18 +5,17 @@
 //  Created by Alexey Manokhin on 17.02.2023.
 //
 
-import Foundation
 
 struct User {
-    private var login: String?
-    private var password: String?
-    private var person: Person?
+     let login: String?
+     let password: String?
+     let person: Person?
 }
 
 struct Person {
-    private let name: String
-    private let secondName: String
-    private let yearsOld: Int
-    private let city: String
-    private let activity: String
+     let name: String
+     let secondName: String
+     let yearsOld: Int
+     let city: String
+     let activity: String
 }
