b931629635cc6086ee903da0a8abe08c6083e86f
Manohin <111537419+Manohin@users.noreply.github.com>
2023-02-21 09:56:14 +0300

    Minor fix

--- LoginApp/ViewControllers/FirstAboutViewController.swift
+++ LoginApp/ViewControllers/FirstAboutViewController.swift
@@ -21,10 +21,8 @@ final class FirstAboutViewController: UIViewController {
         photoImageView.layer.cornerRadius = photoImageView.frame.height / 2
         nameLabel.font = .boldSystemFont(ofSize: 24.0)
         nameLabel.text = user.person.fullName
-        let aboutText = "Имя: \(user.person.name)\nФамилия: \(user.person.secondName)\nВозраст: \(user.person.yearsOld) года\nСтрана проживания: \(user.person.country)\nГород: \(user.person.city)\n\nУзнать больше можно, нажав по вкладке 'Подробнее' в правом нижнем углу экрана 😉"
-        
         photoImageView.layer.cornerRadius = photoImageView.frame.height / 2
         aboutLabel.text = ""
-        aboutLabel.animation(typing: "\(aboutText)", duration: 0.03)
+        aboutLabel.animation(typing: "\(user.person.about)", duration: 0.03)
     }
 }
